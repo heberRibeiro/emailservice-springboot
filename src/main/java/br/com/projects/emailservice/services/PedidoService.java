5582c7e48d50f6d1fba72e96d04971d6515bd4bb
Heber Ribeiro <heber.r@outlook.com>
2020-01-08 11:40:31 -0300

    PedidoService da camada de serviço

---
+++ src/main/java/br/com/projects/emailservice/services/PedidoService.java
@@ -0,0 +1,56 @@
+package br.com.projects.emailservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.projects.emailservice.domain.Pedido;
+import br.com.projects.emailservice.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	public List<Pedido> findAll() {
+		return pedidoRepository.findAll();
+	}
+
+	public Pedido findById(Long id) {
+		Optional<Pedido> pedido = pedidoRepository.findById(id);
+		return pedido.get(); // If a value is present, returns the value, otherwise throws
+								// NoSuchElementException.
+	}
+
+	public Pedido insert(Pedido pedido) {
+		return pedidoRepository.save(pedido);
+	}
+
+	public void delete(Long id) {
+		pedidoRepository.deleteById(id);
+	}
+
+	public Pedido update(Long id, Pedido pedido) {
+		/*
+		 * Returns a reference to the entity with the given identifier. Is very likely
+		 * to return an instance and throw an EntityNotFoundException.
+		 */
+		Pedido obj = pedidoRepository.getOne(id);
+
+		/*
+		 * Modify the pedido entity (obj) in database from pedido object.
+		 */
+		obj.setProduto(pedido.getProduto());
+		obj.setCliente(pedido.getCliente());
+		obj.setData(pedido.getData());
+
+		/*
+		 * Save the modified entity in database
+		 */
+		return pedidoRepository.save(obj);
+
+	}
+}
